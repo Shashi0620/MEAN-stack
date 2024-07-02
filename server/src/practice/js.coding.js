c40aee7ca360c229f65629f0e7b4a83754cc0ef6
Shashi0620 <shashikiranss007@gmail.com>
2024-07-02 22:40:22 +0530

    commit

--- server/src/practice/js.coding.js
+++ server/src/practice/js.coding.js
@@ -47,5 +47,53 @@ function palindrome(str){
 }
 console.log(palindrome('racecar'))
 
+// number of occurance
+function numberOfOccurance(arr){
+  let count = {}
+  for(let element of arr){
+    if(count[element]){
+      count[element] += 1      
+     } else {
+       count[element] = 1;
+     }
+  }
+  return count;
+}
+console.log(numberOfOccurance([1,1,2,2,3,4,5,3]))
 
+// common elements
+function findCommonElectems(arr1,arr2){
+  let commonElements = []
+  for(let ele of arr1){
+    if(arr2.includes(ele)){
+      commonElements.push(ele);
+    }
+  }
+  return commonElements;
+}
+console.log(findCommonElectems([1,2,3,4],[1,5,4,3]))
 
+//factorial
+
+function factorial(num){
+  let fact;
+  if(num === 0){
+    return 1
+  } else {
+    fact = num * factorial(num - 1)
+    return fact;
+  }
+}
+
+console.log(factorial(3))
+
+//FIBBONACCISERIES
+function fibbonacciSeries(num){
+  let series = [0,1];
+  for(let i = 2; i<num; i++){
+    series[i] = series[i-1] + series[i-2]
+  }
+  return series;
+}
+
+console.log(fibbonacciSeries(4))
