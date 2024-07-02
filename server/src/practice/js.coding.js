b4d6ee8c5bba875f0910d0e46605fc4da80cd4ca
Shashi0620 <shashikiranss007@gmail.com>
2024-07-02 13:29:32 +0530

    js coding

--- server/src/practice/js.coding.js
+++ server/src/practice/js.coding.js
@@ -13,3 +13,39 @@ const reverseAString = (input) => {
  
  var input = prompt("Enter a string?");
  reverseAString(input)
+
+ function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+console.log(reverseString("hello"));
+ 
+// remove duplicates
+ function findDuplicate(arr){
+  let duplicateValue = []
+  for(let i=0; i<=arr.length-1; i++){
+    if(!duplicateValue.includes(arr[i])){
+       duplicateValue.push(arr[i]);
+     }
+  }
+  return duplicateValue;
+}
+
+console.log(findDuplicate([1,1,2,3,2,3]))
+
+//palindrome
+function palindrome(str){
+  let reversedString =''
+  for(let i=str.length-1; i>=0; i--){
+    reversedString += str[i]
+  }
+  if(str == reversedString){
+    return `${str} is palindrome`
+  } else {
+  return `${str} is not palindrome`
+    
+  }
+}
+console.log(palindrome('racecar'))
+
+
+
